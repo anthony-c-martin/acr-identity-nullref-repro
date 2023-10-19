568364373a68189927dce637006e8d2b29bc074f
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-10-18 20:13:18 -0400

    Repro

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Containers.ContainerRegistry;
+
+var client = new ContainerRegistryContentClient(
+    new Uri("https://mcr.microsoft.com/"),
+    "bicep/ai/cognitiveservices");
+
+await client.DownloadBlobContentAsync(
+    "sha256:ee1bd6b3e57679e1a3677fabdb9fe3fb3cbc1851b9ba121d2034573ce449507d",
+    CancellationToken.None);
+
+Console.WriteLine("Success!");
